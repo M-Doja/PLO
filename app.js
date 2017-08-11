3d2161b3034cfce26f48e793ab65ac1d10c3e0d7
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-08-10 19:26:01 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const express               = require('express'),
+      path                  = require('path'),
+      fs                    = require('fs'),
+      favicon               = require('serve-favicon'),
+      logger                = require('morgan'),
+      mongoose              = require('mongoose'),
+      passport              = require('passport'),
+      LocalStrategy         = require('passport-local'),
+      passportLocalMongoose = require('passport-local-mongoose'),
+      cookieParser          = require('cookie-parser'),
+      multer                = require('multer'),
+      flash                 = require('connect-flash'),
+      bodyParser            = require('body-parser');
+
+const index = require('./routes/index'),
+      users = require('./routes/users');
+
+var app = express();
+
+
+app.use(flash());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(require('express-session')({
+  secret: "fuzzywuzzyorwashe",
+  resave: false,
+  saveUninitialized: false
+}));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  next();
+});
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
