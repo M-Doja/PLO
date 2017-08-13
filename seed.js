c94d029c75dbbb986cf0e8b15ae7c12d6e27efaa
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-08-13 13:47:01 -0700

    fixed seed.js file

---
+++ seed.js
@@ -0,0 +1,84 @@
+var pets = [
+  {
+    id:1,
+    name: 'Fido',
+    age: 12,
+    type: 'dog',
+    breed: 'Mutt',
+    gender: 'male',
+    img: 'Img url goes here',
+    ownerId: 100
+  },
+  {
+    id:2,
+    name: 'Smokey',
+    age: 9,
+    type: 'cat',
+    breed: 'Russian Grey',
+    gender: 'male',
+    img: 'Img url goes here',
+    ownerId: 101
+  },
+  {
+    id:3,
+    name: 'Wookie',
+    age: 9,
+    type: 'cat',
+    breed: 'Mongral',
+    gender: 'male',
+    img: 'Img url goes here',
+    ownerId: 103
+  },
+  {
+    id:4,
+    name: 'Herkimer',
+    age: 16,
+    type: 'lizard',
+    breed: 'Leopard Gecko',
+    gender: 'male',
+    img: 'Img url goes here',
+    ownerId: 104
+  },
+  {
+    id:5,
+    name: 'Asia',
+    age: 12,
+    type: 'dog',
+    breed: 'Husky',
+    gender: 'female',
+    img: 'Img url goes here',
+    ownerId: 105
+  },
+  {
+    id:6,
+    name: 'Shelby',
+    age: 15,
+    type: 'dog',
+    breed: 'Black Lab',
+    gender: 'female',
+    img: 'Img url goes here',
+    ownerId: 106
+  },
+  {
+    id:7,
+    name: 'Bibo',
+    age: 12,
+    type: 'bird',
+    breed: 'Parakeet',
+    gender: 'male',
+    img: 'Img url goes here',
+    ownerId: 107
+  },
+  {
+    id:8,
+    name: 'BJ',
+    age: 8,
+    type: 'dog',
+    breed: 'Mutt',
+    gender: 'male',
+    img: 'Img url goes here',
+    ownerId: 108
+  },
+];
+
+module.exports = pets;
